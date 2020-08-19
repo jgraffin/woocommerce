5de663c395ab11388c54b2d23318f042aff13747
Julio Graffin <julliograffin@gmail.com>
2020-08-19 01:45:24 -0300

    Fazendo modal

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import CarouselContainer from './components/organisms/carouselContainer';
 import Header from './components/organisms/header';
-import Modal from './components/organisms/modal';
 import './sass/App.scss';
 
 const App = () => {
-  const [modalOpen, setModalOpen] = useState(false);
   return (
     <Router>
       <>
         <div className="app-root">
-          <button
-            className="a-button a-button--modal-open"
-            onClick={() => setModalOpen(true)}
-          >
-            Abrir Menu
-          </button>
-          <button
-            className="a-button a-button--modal-close"
-            onClick={() => setModalOpen(false)}
-          >
-            Fechar Menu
-          </button>
           <Header userName="Julio Graffin" />
           <CarouselContainer />
-          {modalOpen && <Modal />}
         </div>
       </>
     </Router>
--- frontend/src/components/organisms/Header.jsx
+++ frontend/src/components/organisms/Header.jsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../../images/image-tretislogo-loja.svg';
 import Cart from '../molecules/cart';
 import SearchForm from '../molecules/searchForm';
+import Modal from './modal';
 
 const Header = (props) => {
+  const [modalOpen, setModalOpen] = useState(false);
+  const ModalButton = ({ className = '', click, ...modifier }) => {
+    const clickClassName = click ? `a-button--modal-${click}` : '';
+    const propsButton = {
+      className: `a-button ${className} ${clickClassName}`,
+      ...modifier,
+    };
+    return <button {...propsButton} />;
+  };
+
   return (
     <>
+      <ModalButton click="close" onClick={() => setModalOpen(false)}>
+        Fechar Menu
+      </ModalButton>
+      {modalOpen && <Modal />}
+
       <header className="o-header">
         <section className="o-header__account">
           <div className="container">
@@ -21,6 +37,9 @@ const Header = (props) => {
         </section>
         <section className="o-header__navigation">
           <div className="container">
+            <ModalButton click="open" onClick={() => setModalOpen(true)}>
+              Abrir Menu
+            </ModalButton>
             <h1>
               <Link to="/">
                 <img src={logo} alt="Loja Trétis" />
